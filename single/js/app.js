e6e9c4187f04612b95693e2f4286604f04a07924
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2019-03-06 22:43:09 +0530

    if number is happy or not implemented

--- single/js/app.js
+++ single/js/app.js
@@ -3,7 +3,6 @@ const clickFunction = () => {
 }
 
 const enterKeyFunction = (event) => {
-    debugger
     if (event.which == 13 || event.keyCode == 13) {
         computeFunction();
     }
@@ -12,14 +11,46 @@ const enterKeyFunction = (event) => {
 const computeFunction = () => {
     let str = document.getElementById('ipText').value;
     if (str !== "") {
-        return document.getElementById('opText').innerHTML = str;
-    }
-    else {
+        let num = Number(str);
+        let sq;
+        if (num < 10) {
+            sq = num * num;
+            if (sq < 10) {
+                if (sq === 1) {
+                    return document.getElementById('opText').innerHTML = "Happy Number";
+                } else {
+                    return document.getElementById('opText').innerHTML = "Unhappy Number";
+                }
+            } else {
+                sumOfAllDigitsInANumber(sq);
+            }
+        } else {
+            sumOfAllDigitsInANumber(num);
+        }
+    } else {
         return document.getElementById('opText').innerHTML = "Please enter a number";
     }
 }
 
-function clearFields(){
+function sumOfAllDigitsInANumber(n) {
+    debugger
+    let ar = n.toString().split("");
+    let sum = 0;
+    for (let i = 0; i < ar.length; i++) {
+        sum = sum+Number(ar[i])*Number(ar[i]);
+    }
+    if(sum === 1){
+        return document.getElementById('opText').innerHTML = "Happy Number";
+    }
+    else if(sum > 1 && sum < 10){
+        return document.getElementById('opText').innerHTML = "Unhappy Number";
+    }
+    else{
+        sumOfAllDigitsInANumber(sum);
+    }
+}
+
+function clearFields() {
     document.getElementById('ipText').value = "";
     document.getElementById('opText').innerHTML = "";
 }
